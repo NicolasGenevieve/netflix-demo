7cf599fee2b507425f298a330d78e1dbedd4cbec
Nicolas Genevieve <contact@nicolasgenevieve.com>
2025-06-16 18:30:38 +0200

    Make the logo appears

--- src/components/header/Brand.jsx
+++ src/components/header/Brand.jsx
@@ -1,10 +1,11 @@
 import "./Brand.css";
+import logo from "../../assets/logo-a79c3484.png";
 
 const Brand = () => {
   return (
     <>
       <div className="brandCustom">
-        <img src="../src/assets/logo-a79c3484.png" alt="Logo Netflix" />
+        <img src={logo} alt="Logo Netflix" />
       </div>
     </>
   );
